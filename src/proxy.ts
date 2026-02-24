8de4e3f2f58ec754827726545b0b1e5df96e5ad4
shreyavni <avnireeta@gmail.com>
2026-02-25 02:47:37 +0530

    initial commit

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,31 @@
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)","/"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/"]);
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR", "CATEGORY:PREVIEW"],
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      interval: "1m",
+      max: 100,
+    }),
+  ],
+});
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) 
-    await auth.protect();
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    return new Response(null, { status: 403 });
   }
-);
+  if (!isPublicRoute(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
